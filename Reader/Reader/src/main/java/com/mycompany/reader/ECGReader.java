50707685b83ed94092303a9fa5b08234cec0c194
DanyOctrome <DanyOctrome@gmail.com>
2017-03-29 18:45:55 +0100

    works on Reader

---
+++ Reader/Reader/src/main/java/com/mycompany/reader/ECGReader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.reader;
+
+/**
+ *
+ * @author DanyOctrome
+ */
+public class ECGReader {
+
+    public ECGReader(String filename) {
+        //TODO
+    }
+
+    public int  getNextValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Reader/Reader/src/main/java/com/mycompany/reader/Reader.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.reader;
+
+import org.json.JSONObject;
+import java.lang.System;
+
+/**
+ *
+ * @author DanyOctrome
+ */
+public class Reader {
+
+    static final int samplesPerChunk = 500;
+
+    public static void main(String[] args) {
+        boolean endReader = false;
+        boolean endCicle = false;
+        ECGReader fr = new ECGReader("VitalJacket_ECG.tsv");
+        //JSONObject obj = new JSONObject();
+
+        while (!endReader) {
+            JSONObject stream = new JSONObject();
+            String values = "";
+
+            // TODO: add the header stream
+            // Read from the file and add to the values
+            for (int i = 0; (i < samplesPerChunk) || !endCicle; i++) {
+                int value = fr.getNextValue();
+                if (value == -1) {
+                    endCicle = true;
+                    //endReader ?
+                } else {
+                    values += value;
+                }
+            }
+
+            //TODO: put the values in the stream
+        }
+        System.out.println("Reader closing.");
+    }
+}
