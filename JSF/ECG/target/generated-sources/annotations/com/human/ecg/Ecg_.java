35fa59791b1b9a5a605694050fe2b598c3a4ae0d
DanyOctrome <DanyOctrome@gmail.com>
2017-05-04 11:07:44 +0100

    created reader class

--- JSF/ECG/target/generated-sources/annotations/com/human/ecg/Ecg_.java
+++ JSF/ECG/target/generated-sources/annotations/com/human/ecg/Ecg_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-04T10:00:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-04T10:31:31")
 @StaticMetamodel(Ecg.class)
 public class Ecg_ { 
 
